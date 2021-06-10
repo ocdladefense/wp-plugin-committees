fd8e85e46a50d2f0f7214b2c715340aa4aade447
trevoruehlinx1 <trevoruehlinx1@gmail.com>
2021-06-09 17:09:32 -0700

    update the template.

--- templates/committees.tpl.php
+++ templates/committees.tpl.php
@@ -1,114 +1,197 @@
-<style type="text/css">
-.table-headers {
-    display: none;
-}
 
-#intro {
-    font-size: 18px;
-}
 
-h1 {
-    padding-left: 12px;
-    text-transform: uppercase;
-    letter-spacing: 0.14em;
-    font-size: 1.0em;
-}
-
-h3 {
-    color: #bf9500;
-}
-
-#email {
-    color: rgba(0, 0, 139, 0.8);
-    font-weight: bold;
-}
-
-#name {
-    color: grey;
-}
-
-#city {
-    color: #53638c;
-    font-weight: 600;
-}
-
-p {
-    word-wrap: break-word;
-}
-
-@media screen and (min-width: 800px) {
-    .table-headers {
-        display: table-row;
+<style>
+    .primary {
+        border: #50668F;
     }
-}
+
+    .primary {
+        color: #FFFFFF;
+    }
+
+    .primary,
+    .primary:hover,
+    .primary:focus {
+        background-color: #50668F;
+    }
+
+    .primary {
+        background-color: #0070d2;
+        color: white;
+        transition: all 0.1s;
+        border: 1px solid transparent;
+        text-decoration: none;
+        border-radius: 4px;
+        padding: 9px;
+        margin: 10px;
+    }
 </style>
 
+<style type="text/css">
+    .table-headers {
+        display: none;
+    }
+
+    #intro {
+        font-size: 18px;
+    }
+
+    h1 {
+        padding-left: 12px;
+        text-transform: uppercase;
+        letter-spacing: 0.14em;
+        font-size: 1.0em;
+    }
+
+    h3 {
+        color: #bf9500;
+    }
+
+    #email {
+        color: rgba(0, 0, 139, 0.8);
+        font-weight: bold;
+    }
+
+    #name {
+        color: grey;
+    }
+
+    #city {
+        color: #53638c;
+        font-weight: 600;
+    }
+
+    p {
+        word-wrap: break-word;
+    }
+
+    @media screen and (min-width: 800px) {
+        .table-headers {
+            display: table-row;
+        }
+    }
+</style>
 
 <?php
-// Helper function that determines wether a member entry should be bold based on a Role
-function checkRole($member)
-{
-    $roles = [
-        "Chair", "Co-chair", "Board Liaison", "President",
-        "Vice President", "Executive Director", "Legislative Director"
-    ];
-    if (in_array($member["Role"], $roles) || $member["Name"] == "Bob Thuemmel") {
-        echo 'bold';
-    }
-}
-?>
+/**
+ * The template for displaying all pages
+ *
+ * This is the template that displays all pages by default.
+ * Please note that this is the WordPress construct of pages
+ * and that other 'pages' on your WordPress site may use a
+ * different template.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
+ *
+ * @package WordPress
+ * @subpackage wireframe
+ * @since 1.0
+ * @version 1.0
+ */
 
-<?php $separator = "<span style='color: #bf9500; font-weight: bold;'> | </span>"; ?>
+$committees = get_committees_array();
 
-<div>
-    <h1 class="text-muted ml-5 mt-5 font-weight-bold">Committees & Task Forces</h1>
-</div>
+get_header(); ?>
 
-<div class="row">
-    <div id="intro" class="col-md-5 ml-5 mt-3 mb-5 text-muted">
-        <p style="padding-left: 12px;">OCDLA committees are volunteer driven and are open to interested members. Contact
-            the committee chairs below if you would like to become involved.
-        </p>
-    </div>
-</div>
+<div class="page-wrap wrap">
+    <?php get_sidebar('left'); ?>
 
-<?php if (!isset($committees) || (isset($committees) && count($committees) < 1)) : ?>
-<ul class="table-row">
-    <li>There are no committees to display.</li>
-</ul>
+    <div id="primary" class="basic-page content-area">
+        <main id="main" class="site-main" role="main">
+            <br />
 
-<?php else : ?>
+            <div>
+                <h1 class="text-muted ml-5 mt-5 font-weight-bold">Committees & Task Forces</h1>
+            </div>
 
-<?php foreach ($committees as $committee) : ?>
+            <div class="row">
+                <div id="intro" class="col-md-5 ml-5 mt-3 mb-5 text-muted">
+                    <p style="padding-left: 12px;">OCDLA committees are volunteer driven and are open to interested
+                        members. Contact
+                        the committee chairs below if you would like to become involved.
+                    </p>
+                </div>
+            </div>
 
-<a href="http://sf/page?data=<?php echo $committee["Name"]; ?>">
-    <h3 class="ml-5" style="padding-left: 12px;"><?php print $committee["Name"]; ?></h3>
-</a>
-<div class="w-auto ml-5 mt-3 mb-5 mr-5">
+            <?php if (!isset($committees) || (isset($committees) && count($committees) < 1)) : ?>
+                <ul class="table-row">
+                    <li>There are no committees to display.</li>
+                </ul>
 
-    <?php $members = $committee["members"]; ?>
-    <?php foreach ($members as $member) : ?>
+            <?php else : ?>
+                
+                <?php foreach ($committees as $committee) : ?>
 
-    <span style="padding-left: 12px;">
-        <a href="https://members.ocdla.org/directory/member/<?php print $member["Id"]; ?>" target="_blank" id="name">
-            <?php print $member["Name"] . $separator; ?>
-        </a>
-    </span>
-    <span style="font-weight:<?php checkRole($member) ?>;">
-        <?php substr($member["Role"], -4) != "mber" ? print $member["Role"] . $separator : ""; ?>
-    </span>
-    <span id="city">
-        <?php $member["City"] != null ? print $member["City"] . $separator : ""; ?>
-    </span>
-    <span>
-        <?php $email = $member["Email"]; ?>
-        <?php echo "<a href='mailto:$email' id='email'>Email</a>"; ?>
-    </span>
-    <br>
+                    <a href="/page?data=<?php echo $committee["Name"]; ?>">
+                        <h3 class="ml-5" style="padding-left: 12px;"><?php print $committee["Name"]; ?></h3>
+                    </a>
 
-    <?php endforeach; ?>
+                    <div class="w-auto ml-5 mt-3 mb-5 mr-5">
 
-</div>
+                        <?php foreach ($committee["members"] as $member) :
 
-<?php endforeach; ?>
-<?php endif; ?>
+                            $hasPosition = hasPosition($member);
+                            $directoryLink = OCDLA_MEMBERSHIP_DIRECTORY_DOMAIN . "/directory/member/{$member['Id']}";
+                            $separator = "<span style='color: #bf9500; font-weight: bold;'> | </span>";
+                            $email = $member["Email"];
+                        ?>
+                            
+
+                            <span style="padding-left: 12px;">
+                                <a href="<?php print $directoryLink; ?>" target="_blank">
+                                    <?php print $member["Name"]; ?>
+                                    <span style='color: #bf9500; font-weight: bold;'> | </span>
+                                </a>
+                            </span>
+
+                            <span style="font-weight:<?php print $hasPosition ? "bold" : ""; ?>;">
+                                <?php if($hasPosition) :
+                                    print $member["Role"];
+                                ?>
+                                    <span style='color: #bf9500; font-weight: bold;'> | </span>
+
+                                <?php endif; ?>
+
+                            </span>
+
+                            <span id="city">
+                                <?php if($member["City"] != null) :
+                                    print $member["City"];
+                                ?>
+                                    <span style='color: #bf9500; font-weight: bold;'> | </span>
+                                    
+                                <?php endif; ?>
+                            </span>
+
+                            <span>
+                                <?php echo "<a href='mailto:$email' id='email'>Email</a>"; ?>
+                            </span>
+
+                            <br />
+                                
+                        <?php endforeach; ?>
+
+                    </div>
+
+                    <div style="margin-top:5px;">
+                        <?php $linkInfo = getCommitteeLink($committee["Name"]); ?>
+
+                        <a href="<?php print get_site_url() . $linkInfo['url']; ?>"><?php print $linkInfo["text"]; ?></a>
+                    </div>
+
+
+                <?php endforeach; ?>
+
+                <p>Check the OCDLA&nbsp;<a href="https://cases.ocdla.org/calendar/">calendar</a>&nbsp;for upcoming committee meetings.</p>
+                
+            <?php endif; ?>
+
+        </main><!-- #main -->
+
+    </div><!-- #primary -->
+
+    <?php get_sidebar('right'); ?>
+
+</div><!-- .wrap -->
+
+<?php get_footer();
